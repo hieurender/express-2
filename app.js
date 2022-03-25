8ce268a308b3e68f93267eac3b54b0d44d5e7dd2
Hieu <hieu@render.com>
2022-03-24 20:52:12 -0700

    multi ports

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.send("Hello there 2!"));
 app.get("/envs", (req, res) => res.send(`SECRET: ${process.env['SECRET']}; SECRET_2: ${process.env['SECRET_2']}`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+[port, 8080, 9000].forEach((p, i) => {
+  app.listen(p, () => console.log(`Example app ${i} listening on port ${p}!`));
+});
